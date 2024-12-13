d13791391a4ee8346dfe01044d7577e37d03d9ff
Steve Dondley <s@dondley.com>
2024-12-13 13:07:55 -0500

    make json output consistent from generation to generation

--- lib/KarabinerGenerator/Template.pm
+++ lib/KarabinerGenerator/Template.pm
@@ -24,6 +24,38 @@ sub _info {
     warn "$msg\n" unless $ENV{QUIET};
 }
 
+# Fixed order list for JSON keys
+my @key_order = qw(
+    title rules description manipulators type conditions from to key_code shell_command name value
+);
+my %key_order_map = map { $key_order[$_] => $_ } 0..$#key_order;
+
+sub _ordered_keys {
+    my ($hash) = @_;
+    return sort {
+        ($key_order_map{$a} // 999) <=> ($key_order_map{$b} // 999)
+        || $a cmp $b
+    } keys %$hash;
+}
+
+sub _ordered_encode {
+    my ($data) = @_;
+
+    if (ref $data eq 'HASH') {
+        return '{' . join(',',
+            map {
+                JSON::encode_json("$_") . ':' . _ordered_encode($data->{$_})
+            } _ordered_keys($data)
+        ) . '}';
+    }
+    elsif (ref $data eq 'ARRAY') {
+        return '[' . join(',', map { _ordered_encode($_) } @$data) . ']';
+    }
+    else {
+        return JSON::encode_json($data);
+    }
+}
+
 sub process_templates {
     my ($template_dir, $config, $output_dir) = @_;
 
@@ -66,7 +98,7 @@ sub process_templates {
             croak "Template processing failed: " . $tt->error() . "\n";
         }
 
-        # Validate and format JSON
+        # Parse JSON and re-encode with ordered keys
         my $json_obj;
         eval {
             $json_obj = decode_json($output);
@@ -78,10 +110,10 @@ sub process_templates {
         # Create output file path
         my $output_file = File::Spec->catfile($output_dir, "$base_name.json");
 
-        # Write formatted JSON to output file
+        # Write ordered JSON to output file
         open(my $fh, '>', $output_file)
             or croak "Cannot open output file $output_file: $!\n";
-        print $fh JSON->new->pretty->canonical->encode($json_obj);
+        print $fh _ordered_encode($json_obj);
         close($fh);
 
         push @generated_files, $output_file;
